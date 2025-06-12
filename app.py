325908b1ece86be9a254eba7d383017052303e3e
Mac <mac@MacBook-Air-cua-Mac.local>
2025-06-12 10:00:02 +0700

    add root route / for health check

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return 'AlexWebhookBot is running!'
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
@@ -10,3 +14,4 @@ def webhook():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
+
